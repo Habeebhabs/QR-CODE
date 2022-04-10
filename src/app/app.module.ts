733047324eeb994b3461713e261f2f2a6843f472
Habeebhabs <Habeebhabs123@gmail.com>
2022-04-10 22:19:57 +0530

    New Components and firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { QRCodeModule } from 'angularx-qrcode';
+import { NavbarComponent } from './navbar/navbar.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    QRCodeModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'qrcode-4c943',
+    appId: '1:289556535642:web:1377d78a7b171e37b5bb63',
+    storageBucket: 'qrcode-4c943.appspot.com',
+    apiKey: 'AIzaSyBiKZm2_ExDe2HSe77u6qh4DS5r31UwaMo',
+    authDomain: 'qrcode-4c943.firebaseapp.com',
+    messagingSenderId: '289556535642',
+  },
   production: true
 };
