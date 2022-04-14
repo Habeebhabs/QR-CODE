b0b005f3af95a23a8ac35ef1161a5d7318e35d75
Habeebhabs <Habeebhabs123@gmail.com>
2022-04-15 00:03:56 +0530

    navbar change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,24 +1,16 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit,AfterViewInit {
+export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  something: boolean = false;
   ngOnInit(): void {
   }
-  
-  something: boolean = false;
-
-  ngAfterViewInit(): void {
-    setTimeout(() => this.something = true,3000);
-    setTimeout(() => this.something = false,6000)
-  }
-
-  
 
 }
