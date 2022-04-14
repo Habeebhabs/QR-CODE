a1969177db2b7b9417129e09a0e181a6138f5884
Habeebhabs <Habeebhabs123@gmail.com>
2022-04-14 23:13:18 +0530

    user icon added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit,AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => this.something = true,3000);
+    setTimeout(() => this.something = false,6000)
+  }
+
+  something: boolean = false;
 
 }
